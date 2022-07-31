fbd5e339aa038c2d4796e04fe8faf8b679286e7b
Eduard Lozhkin <lozhkinea@yandex.ru>
2022-07-31 10:58:06 +0300

    fix secret_key

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,13 +1,17 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = "hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
